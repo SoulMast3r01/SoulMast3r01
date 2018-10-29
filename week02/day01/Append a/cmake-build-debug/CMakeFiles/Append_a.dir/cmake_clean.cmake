cade906b21a08e199cefc1dcc49b6e18145aa280
SoulMast3r01 <akoswb@gmail.com>
2018-10-29 14:54:22 +0100

    chore

---
+++ week02/day01/Append a/cmake-build-debug/CMakeFiles/Append_a.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Append_a.dir/main.cpp.obj"
+  "Append_a.pdb"
+  "Append_a.exe"
+  "Append_a.exe.manifest"
+  "libAppend_a.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Append_a.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

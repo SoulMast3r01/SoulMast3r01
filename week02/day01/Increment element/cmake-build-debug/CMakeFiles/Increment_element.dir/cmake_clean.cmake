94f83bf8172e97298c20a7bdd11b2ed1b7db9374
SoulMast3r01 <akoswb@gmail.com>
2018-10-29 16:55:10 +0100

    chore

---
+++ week02/day01/Increment element/cmake-build-debug/CMakeFiles/Increment_element.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Increment_element.dir/main.cpp.obj"
+  "Increment_element.pdb"
+  "Increment_element.exe"
+  "Increment_element.exe.manifest"
+  "libIncrement_element.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Increment_element.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

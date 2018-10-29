b8ad191b7cecc70382f57ce9b272ed3da988744d
SoulMast3r01 <akoswb@gmail.com>
2018-10-29 17:19:24 +0100

    chore

---
+++ week02/day01/Print elements/cmake-build-debug/CMakeFiles/Print_elements.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Print_elements.dir/main.cpp.obj"
+  "Print_elements.pdb"
+  "Print_elements.exe"
+  "Print_elements.exe.manifest"
+  "libPrint_elements.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Print_elements.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

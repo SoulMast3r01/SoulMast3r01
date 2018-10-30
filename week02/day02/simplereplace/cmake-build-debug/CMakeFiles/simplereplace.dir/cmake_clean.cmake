43482d1a9afebced7f454398fce9a2f042a543d1
SoulMast3r01 <akoswb@gmail.com>
2018-10-30 11:27:12 +0100

    chore

---
+++ week02/day02/simplereplace/cmake-build-debug/CMakeFiles/simplereplace.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/simplereplace.dir/main.cpp.obj"
+  "simplereplace.pdb"
+  "simplereplace.exe"
+  "simplereplace.exe.manifest"
+  "libsimplereplace.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/simplereplace.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

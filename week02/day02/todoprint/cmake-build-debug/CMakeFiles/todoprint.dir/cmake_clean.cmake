8811552f2829e216d497dd90aa2b0ed1889be165
SoulMast3r01 <akoswb@gmail.com>
2018-10-30 13:53:42 +0100

    chore

---
+++ week02/day02/todoprint/cmake-build-debug/CMakeFiles/todoprint.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/todoprint.dir/main.cpp.obj"
+  "todoprint.pdb"
+  "todoprint.exe"
+  "todoprint.exe.manifest"
+  "libtodoprint.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/todoprint.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

21502f71a81935e54b86a1df39f81424044bc93c
SoulMast3r01 <akoswb@gmail.com>
2018-10-30 13:13:17 +0100

    chore

---
+++ week02/day02/takeslonger/cmake-build-debug/CMakeFiles/takeslonger.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/takeslonger.dir/main.cpp.obj"
+  "takeslonger.pdb"
+  "takeslonger.exe"
+  "takeslonger.exe.manifest"
+  "libtakeslonger.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/takeslonger.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ week02/day02/urlfixer/cmake-build-debug/CMakeFiles/urlfixer.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/urlfixer.dir/main.cpp.obj"
+  "urlfixer.pdb"
+  "urlfixer.exe"
+  "urlfixer.exe.manifest"
+  "liburlfixer.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/urlfixer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

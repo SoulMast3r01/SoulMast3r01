0387bf6b7a8d4f2b28b910ce84b6627d15029c0b
SoulMast3r01 <akoswb@gmail.com>
2018-10-31 10:25:08 +0100

    chore

---
+++ week02/day03/Practise01/cmake-build-debug/CMakeFiles/Practise01.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Practise01.dir/main.cpp.obj"
+  "Practise01.pdb"
+  "Practise01.exe"
+  "Practise01.exe.manifest"
+  "libPractise01.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Practise01.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

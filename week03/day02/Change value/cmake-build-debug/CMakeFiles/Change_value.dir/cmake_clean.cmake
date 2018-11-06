2fc9d46523983501575157153055b2a2e50cef88
SoulMast3r01 <akoswb@gmail.com>
2018-11-06 13:34:14 +0100

    chore

---
+++ week03/day02/Change value/cmake-build-debug/CMakeFiles/Change_value.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Change_value.dir/main.cpp.obj"
+  "Change_value.pdb"
+  "Change_value.exe"
+  "Change_value.exe.manifest"
+  "libChange_value.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Change_value.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

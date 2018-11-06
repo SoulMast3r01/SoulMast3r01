e341215f29645351cfcab5248a52f97ab44da71e
SoulMast3r01 <akoswb@gmail.com>
2018-11-06 15:29:41 +0100

    chore

---
+++ week03/day02/Print adresses/cmake-build-debug/CMakeFiles/Print_adresses.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Print_adresses.dir/main.cpp.obj"
+  "Print_adresses.pdb"
+  "Print_adresses.exe"
+  "Print_adresses.exe.manifest"
+  "libPrint_adresses.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Print_adresses.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

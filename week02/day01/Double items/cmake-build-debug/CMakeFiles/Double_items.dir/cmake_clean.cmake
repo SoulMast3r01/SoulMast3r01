bb37b29d266cae384befe0f968dad542e0d7a989
SoulMast3r01 <akoswb@gmail.com>
2018-11-08 14:37:16 +0100

    chore

---
+++ week02/day01/Double items/cmake-build-debug/CMakeFiles/Double_items.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Double_items.dir/main.cpp.obj"
+  "Double_items.pdb"
+  "Double_items.exe"
+  "Double_items.exe.manifest"
+  "libDouble_items.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Double_items.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

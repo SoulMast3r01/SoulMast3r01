7909dcc443721273e1b32745a327d55f835694eb
SoulMast3r01 <akoswb@gmail.com>
2018-11-12 16:42:15 +0100

    chore

---
+++ week04/day01/Write single line/cmake-build-debug/CMakeFiles/Write_single_line.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Write_single_line.dir/main.cpp.obj"
+  "Write_single_line.pdb"
+  "Write_single_line.exe"
+  "Write_single_line.exe.manifest"
+  "libWrite_single_line.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Write_single_line.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

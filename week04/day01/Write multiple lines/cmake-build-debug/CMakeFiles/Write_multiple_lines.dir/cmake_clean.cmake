bec2c4779aefb93d76694a083a0636716f6ccbfe
SoulMast3r01 <akoswb@gmail.com>
2018-11-12 17:29:01 +0100

    chore

---
+++ week04/day01/Write multiple lines/cmake-build-debug/CMakeFiles/Write_multiple_lines.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Write_multiple_lines.dir/main.cpp.obj"
+  "Write_multiple_lines.pdb"
+  "Write_multiple_lines.exe"
+  "Write_multiple_lines.exe.manifest"
+  "libWrite_multiple_lines.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Write_multiple_lines.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

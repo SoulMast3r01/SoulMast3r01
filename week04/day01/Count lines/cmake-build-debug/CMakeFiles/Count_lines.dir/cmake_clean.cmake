0a5b84e6ba7b8ded767b6c470512a8085e2416b7
SoulMast3r01 <akoswb@gmail.com>
2018-11-12 15:52:54 +0100

    chore

---
+++ week04/day01/Count lines/cmake-build-debug/CMakeFiles/Count_lines.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Count_lines.dir/main.cpp.obj"
+  "Count_lines.pdb"
+  "Count_lines.exe"
+  "Count_lines.exe.manifest"
+  "libCount_lines.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Count_lines.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

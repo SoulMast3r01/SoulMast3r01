01bf455e39437e31ebe1fdf3b8c29b7a96c998de
SoulMast3r01 <akoswb@gmail.com>
2018-11-13 15:55:46 +0100

    chore

---
+++ week04/day02/Animal/cmake-build-debug/CMakeFiles/Animal.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Animal.dir/main.cpp.obj"
+  "CMakeFiles/Animal.dir/animal.cpp.obj"
+  "Animal.pdb"
+  "Animal.exe"
+  "Animal.exe.manifest"
+  "libAnimal.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Animal.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

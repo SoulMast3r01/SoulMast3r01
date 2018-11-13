601993ebea8fce515f2ebf191f6af641a8116c85
SoulMast3r01 <akoswb@gmail.com>
2018-11-13 14:35:28 +0100

    chore

---
+++ week04/day02/BlogPost/cmake-build-debug/CMakeFiles/BlogPost.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/BlogPost.dir/main.cpp.obj"
+  "CMakeFiles/BlogPost.dir/blogpost.cpp.obj"
+  "BlogPost.pdb"
+  "BlogPost.exe"
+  "BlogPost.exe.manifest"
+  "libBlogPost.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/BlogPost.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

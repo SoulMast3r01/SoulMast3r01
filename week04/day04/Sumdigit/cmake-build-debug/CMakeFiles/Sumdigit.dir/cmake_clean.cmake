d7e7ab21c74cc2cd5d75a3e13ef8853118df4780
SoulMast3r01 <akoswb@gmail.com>
2018-11-15 13:28:46 +0100

    chore

---
+++ week04/day04/Sumdigit/cmake-build-debug/CMakeFiles/Sumdigit.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Sumdigit.dir/main.cpp.obj"
+  "Sumdigit.pdb"
+  "Sumdigit.exe"
+  "Sumdigit.exe.manifest"
+  "libSumdigit.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Sumdigit.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

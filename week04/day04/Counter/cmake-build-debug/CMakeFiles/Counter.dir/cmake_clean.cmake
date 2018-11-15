a23a2f4f5ee76f784bcb7438ad6fa2a6c56241b5
SoulMast3r01 <akoswb@gmail.com>
2018-11-15 11:05:48 +0100

    chore

---
+++ week04/day04/Counter/cmake-build-debug/CMakeFiles/Counter.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Counter.dir/main.cpp.obj"
+  "Counter.pdb"
+  "Counter.exe"
+  "Counter.exe.manifest"
+  "libCounter.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Counter.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

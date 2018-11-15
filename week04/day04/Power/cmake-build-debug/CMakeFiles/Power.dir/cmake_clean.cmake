9cd9045ac97671bbeae8c536c210deef0958d041
SoulMast3r01 <akoswb@gmail.com>
2018-11-15 14:50:28 +0100

    chore

---
+++ week04/day04/Power/cmake-build-debug/CMakeFiles/Power.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Power.dir/main.cpp.obj"
+  "Power.pdb"
+  "Power.exe"
+  "Power.exe.manifest"
+  "libPower.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Power.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

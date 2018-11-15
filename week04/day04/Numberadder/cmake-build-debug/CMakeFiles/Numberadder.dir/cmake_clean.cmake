00732bff18bfbc67bdd064abb929788a1e3c3986
SoulMast3r01 <akoswb@gmail.com>
2018-11-15 12:44:15 +0100

    chore

---
+++ week04/day04/Numberadder/cmake-build-debug/CMakeFiles/Numberadder.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Numberadder.dir/main.cpp.obj"
+  "Numberadder.pdb"
+  "Numberadder.exe"
+  "Numberadder.exe.manifest"
+  "libNumberadder.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Numberadder.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
